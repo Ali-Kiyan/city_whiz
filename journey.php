252731d8c467ed8630a0058e874dd0c5f3d33765
Ali <alikiyand@gmail.com>
2018-02-03 15:45:12 +0000

    journey

---
+++ journey.php
@@ -0,0 +1,30 @@
+<?php require_once "./template/header.html" ?>
+<div class="animated fadeInLeft alert alert-success">Hey Alex!</div>
+<div class="animated fadeInUp alert alert-info center">Please select your journey!</div>
+
+<div class="col-xs-4 col-xs-offset-1 smfont">
+  <div class="form-group">
+    <label for="sel1">Source Station: </label>
+    <select class="form-control" id="sel1">
+      <option>Oxfod Road</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </select>
+  </div>
+</div>
+<div class="col-xs-4 col-xs-offset-2 smfont">
+  <div class="form-group">
+    <label for="sel1">Destination Station: </label>
+    <select class="form-control" id="sel1">
+      <option>Victoria Station</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </select>
+  </div>
+</div>
+
+
+
+<?php require_once "./template/footer.html" ?>
