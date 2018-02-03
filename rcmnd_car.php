32061ce765afe95fbdd4ac3535229e8cb918b01d
Ali <alikiyand@gmail.com>
2018-02-03 17:28:15 +0000

    debug the python script

--- rcmnd_car.php
+++ rcmnd_car.php
@@ -1,3 +1,11 @@
 <?php require_once "./template/header.html" ?>
-
+<div class="animated fadeInLeft alert alert-success">Selecet your journey...</div>
+<div class="row">
+  <div class="container">
+    <div class="col-xs-6"><img src="./assets/images/train_car.png" alt="" class="car animated fadeIn"></div>
+    <div class="col-xs-6"><img src="./assets/images/train_car.png" alt="" class="car animated fadeIn"></div>
+    <img src="./assets/images/tick.png" alt="" class="smimg col-xs-6">
+    <img src="./assets/images/tick.png" alt="" class="smimg col-xs-6">
+  </div>
+</div>
 <?php require_once "./template/footer.html" ?>
