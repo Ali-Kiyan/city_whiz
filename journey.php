a566cd25237745ddd5711e6682002053951695b8
Ali <alikiyand@gmail.com>
2018-02-04 10:02:21 +0000

    updating UI

--- journey.php
+++ journey.php
@@ -1,4 +1,9 @@
 <?php require_once "./template/header.html" ?>
+<?php
+$api_url= "http://localhost:3001/";
+$car_data = file_get_contents($api_url);
+$json = json_decode($car_data, TRUE);
+?>
 <div class="animated fadeInLeft alert alert-success">Hey Alex!</div>
 <div class="animated fadeInUp alert alert-info center">Please select your journey!</div>
 
@@ -6,10 +11,9 @@
   <div class="form-group">
     <label for="sel1">Source Station: </label>
     <select class="form-control" id="sel1">
-      <option>Oxfod Road</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
+      <option><?php echo $json['stations'][1]['station']; ?></option>
+      <option><?php echo $json['stations'][2]['station']; ?></option>
+      <option><?php echo $json['stations'][3]['station']; ?></option>
     </select>
   </div>
 </div>
@@ -17,13 +21,12 @@
   <div class="form-group">
     <label for="sel1">Destination Station: </label>
     <select class="form-control" id="sel1">
-      <option>Victoria Station</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
+      <option><?php echo $json['stations'][2]['station']; ?></option>
+      <option><?php echo $json['stations'][3]['station']; ?></option>
+      <option><?php echo $json['stations'][1]['station']; ?></option>
     </select>
   </div>
-    <a type="submit" href="./rcmnd_car.php" class="btn btn-success right">Submit
+    <a type="submit" href="./rcmnd_car.php" class="btn btn-success col-sm-2 col-xs-12">Submit
     </a>
 </div>
 
--- rcmnd_car.php
+++ rcmnd_car.php
@@ -1,11 +1,24 @@
 <?php require_once "./template/header.html" ?>
+<?php
+$api_url= "http://localhost:3001/";
+$car_data = file_get_contents($api_url);
+$json = json_decode($car_data, TRUE);
+?>
 <div class="animated fadeInLeft alert alert-success">Selecet your journey...</div>
 <div class="row">
-  <div class="container">
-    <div class="col-xs-6"><img src="./assets/images/train_car.png" alt="" class="car animated fadeIn"></div>
-    <div class="col-xs-6"><img src="./assets/images/train_car.png" alt="" class="car animated fadeIn"></div>
-    <img src="./assets/images/tick.png" alt="" class="smimg col-xs-6">
-    <img src="./assets/images/tick.png" alt="" class="smimg col-xs-6">
-  </div>
+    <div class="container">
+      <div class="col-xs-6"><img src="./assets/images/train_car.png" alt="" class="car animated fadeIn col-md-4 col-xs-8">
+      </div>
+      <div class="col-xs-6"> <img src="./assets/images/train_car.png" alt="" class="car animated fadeIn col-md-4 col-xs-8">
+      </div>
+
+    </div>
+   <?php if($json['stations'][1]['recomendation'] == "left") echo '<img src="./assets/images/tickg.gif" alt="" class="col-sm-2 col-sm-offset-1 col-xs-4 col-sm-offset-1">'; ?>
+   <?php if($json['stations'][1]['recomendation'] == "right") echo '<img src="./assets/images/tickg.gif" alt="" class="col-sm-2 col-sm-offset-6 col-xs-4 col-xs-offset-6">'; ?>
+   <br>
+   <br>
+   <br>
 </div>
+<div class="alert alert-warning animated fadeInRight" style="width: 40%;"><?php echo "choose the ".$json['stations'][1]['recomendation']. " carraige"; ?></div>
+
 <?php require_once "./template/footer.html" ?>
